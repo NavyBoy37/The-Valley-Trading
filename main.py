4677bec4dc1dc209b43bb0e56570a9d01fc98f51
NavyBoy37 <joshuayawn.epworth@gmail.com>
2025-02-03 17:28:58 -0500

    Notes(planning.txt): -Added notes in planning and TODO in main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-""" TODO:  Currency is stored in cart by individual denominations but then automatically converted to most efficient denominations 
-        when transactions are made.  Eventually coinage will have weight so if I only have 10 silver and no copper or gold, I want the cart and transactions to reflect that.  
-        Current example below shows problem."""
+""" TODO:  Add lists to each city dictionary for texts to be shown to the player as they enter for the first time.  
+Pull a random one from the list each time they enter the city.  This will be the city's "flavor text.  Specific unique events can come later"""
+
+"""TODO:   Expand main.py to include a loop that allows the player to visit different cities and interact with the market.
+"""
 
 import time
 from basic_town_functions import visit_market, visit_money_exchange
@@ -37,19 +39,20 @@ iron_ore
 player = Player_Initialization()
 wagon = Wagon_Initialization()
 player["city"] = Feldor_Initialization()  # temp
-print(
-    "You finally did it... After years of hard work, you have enough silver to buy a cart."
-)
-time.sleep(2)
-print(". . .")
-print(
-    "It's not much, but it's got wheels.  With "
-    + str(wagon["cart"]["silver"])
-    + " silver, you can finally start your journey.  You set your sights on the horizon."
-)
-##testing zone below
-visit_money_exchange(wagon, player["city"])
-visit_market(wagon, player["city"])
-print("Copper:  " + str(wagon["cart"]["copper"]))
-print("Silver:  " + str(wagon["cart"]["silver"]))
-print("Gold:  " + str(wagon["cart"]["gold"]))
+while True:
+    print(
+        "You finally did it... After years of hard work, you have enough silver to buy a cart."
+    )
+    time.sleep(2)
+    print(". . .")
+    print(
+        "It's not much, but it's got wheels.  With "
+        + str(wagon["cart"]["silver"])
+        + " silver, you can finally start your journey.  You set your sights on the horizon."
+    )
+    ##testing zone below
+    visit_money_exchange(wagon, player["city"])
+    visit_market(wagon, player["city"])
+    print("Copper:  " + str(wagon["cart"]["copper"]))
+    print("Silver:  " + str(wagon["cart"]["silver"]))
+    print("Gold:  " + str(wagon["cart"]["gold"]))
